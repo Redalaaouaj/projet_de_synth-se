d55c51486cd549d5291953ab821e65efe533ca95
RedaLaaouaj <redalaaouaj1@gmail.com>
2024-06-03 15:21:07 +0100

    add login page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,6 +4,7 @@ import RegisterPage from './auth/RegistrePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CarListing from './components/CarListing';
 import Reservation from './components/Reservation';
+import LoginPage from './auth/LoginPage';
 
 function App() {
   const [user, setUser] = useState();
@@ -12,16 +13,19 @@ function App() {
     <AuthContext.Provider value={{ user, setUser }} >
       <BrowserRouter>
         <Routes>
+        <Route path="/admin" element={<AdminRedirect />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path='/' element={<CarListing/>}/>
           <Route path='/reservation' element={<Reservation/>}/>
-          {/* <Route path="/login" element={<LoginPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
+           <Route path="/login" element={<LoginPage />} />
+          {/*<Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
           <Route path="*" element={<h1>not found</h1> } />
         </Routes>
       </BrowserRouter>
     </AuthContext.Provider>
   )
 }
-
+function AdminRedirect() {
+  window.location.href = "http://localhost:8000/admin/login";
+}
 export default App
---
+++ FrontEnd/src/auth/LoginPage.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const LoginPage = () => {
+  const [activeTab, setActiveTab] = useState('user');
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userTel, setUserTel] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userPasswordConfirmation, setUserPasswordConfirmation] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="bg-cover bg-center min-h-screen flex items-center justify-center" style={{ backgroundImage: `url(bg.webp)` }}>
+      <div className="bg-gray-300 bg-opacity-90 p-8 rounded-lg shadow-lg register-form ">
+        <img className="logo-allocazion-register" src='allocazion.png' alt="logo" style={{width:'300px'}}/>
+        <div className="flex justify-center mb-5">
+          <button className={`px-4 py-2 rounded-tl-lg ${activeTab === 'user' ? 'bg-blue-800 text-white' : 'bg-gray-200 text-gray-800'}`} onClick={() => handleTabChange('user')}>
+            Locataire
+          </button>
+          <button className={`px-4 py-2 rounded-tr-lg ${activeTab === 'agence' ? 'bg-blue-800 text-white' : 'bg-gray-200 text-gray-800'}`} onClick={() => handleTabChange('agence')}>
+            Entreprise
+          </button>
+        </div>
+        {activeTab === 'user' && (
+          <form action='/' className="flex flex-col">
+
+            <input
+              type="text"
+              placeholder="Email"
+              value={userEmail}
+              onChange={() => onClick(setUserEmail(e.target.value))}
+              className={`mb-4 px-4 py-2 rounded-md border border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-600 ${userEmail ? 'border-blue-600' : ''}`}
+            />
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              value={userPassword}
+              onChange={() => onClick(setUserPassword(e.target.value))}
+              className={`mb-4 px-4 py-2 rounded-md border border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-600 ${userPassword ? 'border-blue-600' : ''}`}
+            />
+            <button type="submit" className="bg-blue-800 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300">Se connecter</button>
+          </form>
+        )}
+
+
+        <div className="mt-4 text-center">
+          <Link to="/register" className="text-blue-500 hover:underline">Créer un compte</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
